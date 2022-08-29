9f3f21c90d1ec638a8c673dee76204788581cd1c
vendettjesus <91543831+vendettjesus@users.noreply.github.com>
2022-08-28 22:12:31 -0500

    Ejercicio 2 JS

---
+++ Curso-JavaScript/Sintaxis, variables y palabras reservadas de JS/ejercicio2.js
@@ -0,0 +1,11 @@
+
+const libro = {
+    titulo: "Cien años de soledad",
+    autor: "Gabriel García Márquez",
+    ano: 1967,
+    url: "https://docs.google.com/file/d/0B8UVZwG3q97MM2ZzNjQtM3BFUFU/view?resourcekey=0-ZC0XPFNuq0n5vaxQjSQ7Sg"
+}
+const array = ["José de Jesús Alvarado", 34, true, new Date(1988, 4, 7), libro];
+
+
+console.log(array);
